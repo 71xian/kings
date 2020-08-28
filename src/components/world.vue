9af6531ec1a90559a8aa3ad6e3d58464d4d9c5c3
YXCLING <2586846075@qq.com>
2020-08-28 13:41:50 +0800

    上传代码

---
+++ src/components/world.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+      <div id="world"></div>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  main{
+    overflow: hidden;
+    width: 100%;
+  }
+  #world{
+    margin: 3%;
+    width: 94%;
+    height: 700px;
+    background: url("../assets/images/world_p.jpg") no-repeat;
+    background-size: 100%;
+  }
+</style>
